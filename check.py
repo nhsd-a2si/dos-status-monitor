fc2785b556b10e93bd064f09dcd9eee97c30042a
Matt Stibbs <git@stibbsy.co.uk>
2017-09-24 23:14:51 +0100

    Refactor check.py to move database methods into database.py and rename twilio_client to sms_client

--- check.py
+++ check.py
@@ -1,15 +1,14 @@
 import config
-from database import snapshots, add_change
+import database
 import datetime
-import pymongo
 from twilio.rest import Client
 import uec_dos
 
-twilio_client = Client(config.TWILIO_ACCOUNT_SID, config.TWILIO_AUTH_TOKEN)
+sms_client = Client(config.TWILIO_ACCOUNT_SID, config.TWILIO_AUTH_TOKEN)
 
 
 def send_sms(to, sms_text):
-    message = twilio_client.messages.create(
+    message = sms_client.messages.create(
         to,
         body=sms_text,
         from_=config.TWILIO_FROM_NUMBER)
@@ -18,14 +17,19 @@ def send_sms(to, sms_text):
     
     
 def has_status_changed(service_id, new_status):
-    query = {'id': service_id}
-    results = snapshots.find(query).sort([('checkTime', pymongo.DESCENDING)]).limit(1)
+    
+    results = database.get_snapshots_for_service(service_id)
+    
     if results.count() > 0:
+        
         for result in results:
+            
             if result['capacity']['status'] != new_status:
+                
                 print("Status has changed!")
+                
                 data = uec_dos.get_service_by_service_id(service_id)
-                service_updatedBy = data['success']['services'][0]['capacity']['updated']['by']
+                service_updated_by = data['success']['services'][0]['capacity']['updated']['by']
                 service_status = data['success']['services'][0]['capacity']['status']['human']
                 service_name = data['success']['services'][0]['name']
                 service_updated_date = data['success']['services'][0]['capacity']['updated']['date']
@@ -33,32 +37,43 @@ def has_status_changed(service_id, new_status):
                 
                 send_sms(config.MOBILE_NUMBER,
                          f"Capacity changed for {service_name} ({service_id}). \n"
-                         f"It was changed to {service_status} by {service_updatedBy}.")
+                         f"It was changed to {service_status} by {service_updated_by}.")
                 
                 document = {'id': service_id,
                             'name': service_name,
                             'checkTime': datetime.datetime.utcnow(),
                             'status': service_status,
                             'previousStatus': result['capacity']['status'],
-                            'updatedBy': service_updatedBy,
+                            'updatedBy': service_updated_by,
                             'updatedDate': service_updated_date,
                             'updatedTime': service_updated_time
                             }
             
-                add_change(document)
+                database.add_change(document)
+                return
+    
     else:
+    
         print("No previous snapshot available to compare with")
+        return
 
 
 def job():
+    
     services = uec_dos.get_services()
 
     for service in services:
+        
         print(f"{service['id']} - {service['name']} ({service['odsCode']}) - {service['capacity']['status']['human']}")
+
         has_status_changed(service['id'], service['capacity']['status']['human'])
+        
+        document = {'id': service['id'],
+                    'checkTime': datetime.datetime.utcnow(),
+                    'capacity': {
+                        'status': service['capacity']['status']['human'],
+                    }}
+
+        database.add_snapshot(document)
     
-        snapshots.insert({'id': service['id'],
-                          'checkTime': datetime.datetime.utcnow(),
-                          'capacity': {
-                          'status': service['capacity']['status']['human'],
-                          }})
+
--- database.py
+++ database.py
@@ -1,5 +1,6 @@
 import config
 from pymongo import MongoClient
+import pymongo
 
 client = MongoClient(config.MONGODB_URI)
 db = client.get_database()
@@ -9,3 +10,13 @@ changes = db['changes']
 
 def add_change(document):
     changes.insert(document)
+
+
+def add_snapshot(document):
+    snapshots.insert(document)
+
+
+def get_snapshots_for_service(service_id):
+    query = {'id': service_id}
+    results = snapshots.find(query).sort([('checkTime', pymongo.DESCENDING)]).limit(1)
+    return results
