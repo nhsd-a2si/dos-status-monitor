1c1637845c4af7b22c156cfb03c638107d90dae2
Matt Stibbs <git@stibbsy.co.uk>
2017-09-24 22:27:19 +0100

    Store a document in a 'changes' collection every time a status change is detected

--- check.py
+++ check.py
@@ -1,5 +1,5 @@
 import config
-from database import snapshots
+from database import snapshots, add_change
 import datetime
 import pymongo
 from twilio.rest import Client
@@ -28,9 +28,24 @@ def has_status_changed(service_id, new_status):
                 service_updatedBy = data['success']['services'][0]['capacity']['updated']['by']
                 service_status = data['success']['services'][0]['capacity']['status']['human']
                 service_name = data['success']['services'][0]['name']
+                service_updated_date = data['success']['services'][0]['capacity']['updated']['date']
+                service_updated_time = data['success']['services'][0]['capacity']['updated']['time']
+                
                 send_sms(config.MOBILE_NUMBER,
                          f"Capacity changed for {service_name} ({service_id}). \n"
                          f"It was changed to {service_status} by {service_updatedBy}.")
+                
+                document = {'id': service_id,
+                            'name': service_name,
+                            'checkTime': datetime.datetime.utcnow(),
+                            'status': service_status,
+                            'previousStatus': result['capacity']['status'],
+                            'updatedBy': service_updatedBy,
+                            'updatedDate': service_updated_date,
+                            'updatedTime': service_updated_time
+                            }
+            
+                add_change(document)
     else:
         print("No previous snapshot available to compare with")
 
--- database.py
+++ database.py
@@ -4,3 +4,8 @@ from pymongo import MongoClient
 client = MongoClient(config.MONGODB_URI)
 db = client.get_database()
 snapshots = db['snapshots']
+changes = db['changes']
+
+
+def add_change(document):
+    changes.insert(document)
