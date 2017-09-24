71d26e0d9fe6ed2d0cbe05bfd8e0f1fe7ec72f6d
Matt Stibbs <git@stibbsy.co.uk>
2017-09-24 19:11:10 +0100

    Rearrange config settings into groups in config.py

--- config.py
+++ config.py
@@ -1,12 +1,20 @@
 import os
 
+# Core App Settings
+MONGODB_URI = os.environ.get('MONGODB_URI')
+CHECK_RATE_MINUTES = int(os.environ.get('CHECK_RATE_MINUTES', '5'))
+
+# UEC DoS Settings
 UEC_DOS_USERNAME = os.environ.get('UEC_DOS_USERNAME')
 UEC_DOS_PASSWORD = os.environ.get('UEC_DOS_PASSWORD')
-TWILIO_ACCOUNT_SID= os.environ.get('TWILIO_ACCOUNT_SID')
-TWILIO_AUTH_TOKEN= os.environ.get('TWILIO_AUTH_TOKEN')
-MONGODB_URI= os.environ.get('MONGODB_URI')
 UEC_DOS_BASE_URL = os.environ.get('UEC_DOS_BASE_URL')
 UEC_DOS_POSTCODE = os.environ.get('UEC_DOS_POSTCODE')
 UEC_DOS_SERVICE_TYPES = os.environ.get('UEC_DOS_SERVICE_TYPES')
+
+# SMS Settings
+TWILIO_ACCOUNT_SID = os.environ.get('TWILIO_ACCOUNT_SID')
+TWILIO_AUTH_TOKEN = os.environ.get('TWILIO_AUTH_TOKEN')
 MOBILE_NUMBER = os.environ.get('MOBILE_NUMBER')
-CHECK_RATE_MINUTES = int(os.environ.get('CHECK_RATE_MINUTES', '5'))
+
+# Email Settings
+SENDGRID_API_KEY = os.environ.get('SENDGRID_API_KEY')
