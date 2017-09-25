dd3c40a9ab2e5f9e8513c61352889b86ef385f81
Matt Stibbs <git@stibbsy.co.uk>
2017-09-25 22:17:36 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -2,7 +2,11 @@ import datetime
 
 
 def remove_1_hour_from_time_string(current_time: str) -> str:
-    # Fix the incorrect service_updated_time by subtracting an hour from the supplied time.
+    """
+    Take a time represented as HH:MM string, and return the same format string with 1 hour subtracted
+    :param current_time: str
+    :return new_time: str
+    """
     time_tup = datetime.datetime.strptime(current_time, '%H:%M')
     new_time = time_tup - datetime.timedelta(hours=1)
     return datetime.datetime.strftime(new_time, '%H:%M')
