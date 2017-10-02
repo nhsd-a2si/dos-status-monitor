7085d7bf3eb6c07108d0ffda036ff715fa37f352
Matt Stibbs <git@stibbsy.co.uk>
2017-10-02 17:59:53 +0100

    Update methods in uec_dos.py to take parameters, and add type hinting

--- uec_dos.py
+++ uec_dos.py
@@ -8,10 +8,10 @@ s.auth = (config.UEC_DOS_USERNAME,
           config.UEC_DOS_PASSWORD)
 
 
-def get_services():
+def get_services(postcode: str, search_distance: int, service_types: list, number_per_type: int) -> dict:
     url = f'{config.UEC_DOS_BASE_URL}/app/controllers/api/v1.0/services/' \
-          f'byServiceType/TEST/{config.UEC_DOS_POSTCODE}/{config.UEC_DOS_SEARCH_DISTANCE}/0/0/0/0/' \
-          f'{config.UEC_DOS_SERVICE_TYPES}/{config.UEC_DOS_NUMBER_PER_TYPE}'
+          f'byServiceType/TEST/{postcode}/{search_distance}/0/0/0/0/' \
+          f'{service_types}/{number_per_type}'
     
     r = s.get(url)
     data = r.json()
@@ -24,6 +24,6 @@ def get_services():
         raise requests.RequestException("Request failed")
     
 
-def get_service_by_service_id(service_id):
+def get_service_by_service_id(service_id: str) -> dict:
     result = s.get(f'https://uat.pathwaysdos.nhs.uk/app/controllers/api/v1.0/services/byServiceId/{service_id}')
     return result.json()
