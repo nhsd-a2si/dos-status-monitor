4ce558180fd254fc10a44399629311482799ba4d
Matt Stibbs <git@stibbsy.co.uk>
2017-12-22 16:33:07 +0000

    Update print line

--- dos_status_monitor/dos_status_monitor.py
+++ dos_status_monitor/dos_status_monitor.py
@@ -98,8 +98,7 @@ def job(probe):
     print(f"Took snapshot for {len(services)} services")
 
     for service in services:
-        print(service)
-        print(service['capacity']['status']['human'])
+        print(f"{service['name']}{service['capacity']['status']['human']}")
         has_status_changed(service['id'], service['capacity']['status']['human'])
 
         document = {'id': service['id'],
