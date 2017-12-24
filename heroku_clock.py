d985aec8715337d8f3add3ba52f0d23654c4b30f
Matt Stibbs <git@stibbsy.co.uk>
2017-12-24 11:36:15 +0000

    Refactor clock code to be more consistent and use better names

--- heroku_clock.py
+++ heroku_clock.py
@@ -3,30 +3,43 @@ import time
 import redis
 from rq import Queue
 
-import dos_status_monitor
 from dos_status_monitor import dos_status_monitor, probes, config
-
-print(f"{len(probes.get_probe_list())} probes configured to run "
-      f"every {config.CHECK_RATE_MINUTES} minute(s)")
 
 # Set up RQ queue
 conn = redis.from_url(config.REDIS_URL)
 q = Queue(connection=conn)
 
+print(f"{len(probes.get_probe_list())} probes configured to run "
+      f"every {config.CHECK_RATE_MINUTES} minute(s)")
 
-def add_job():
+
+def add_search_job():
     probe_list = probes.get_probe_list()
     
     for probe in probe_list:
         print("Adding job to queue")
-        q.enqueue(dos_status_monitor.job,
+        q.enqueue(dos_status_monitor.run_search,
                   probe,
                   ttl=f'{config.CHECK_RATE_MINUTES}m')
-    
 
-schedule.every(config.CHECK_RATE_MINUTES).minutes.do(add_job)
 
-add_job()
+def add_service_jobs():
+    service_list = probes.get_watched_service_list()
+    print(f'Watched Services: {service_list.count()}')
+    for service in service_list:
+        service_id = service['id']
+        print(f"Adding job for {service_id}")
+        q.enqueue(dos_status_monitor.check_single_service,
+                  service_id,
+                  ttl=f'{config.CHECK_RATE_MINUTES}m')
+
+
+add_search_job()
+add_service_jobs()
+
+schedule.every(config.CHECK_RATE_MINUTES).minutes.do(add_search_job)
+schedule.every(config.CHECK_RATE_MINUTES).minutes.do(add_service_jobs)
+
 
 while True:
     print(f"Ping!")
