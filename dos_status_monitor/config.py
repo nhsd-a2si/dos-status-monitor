52cdc289e4dfa1f371cb255f531fa78b0c0ab3a3
Matt Stibbs <git@stibbsy.co.uk>
2017-12-24 19:56:51 +0000

    Add new config setting for Slack Webhook URL

--- dos_status_monitor/config.py
+++ dos_status_monitor/config.py
@@ -11,6 +11,8 @@ UEC_DOS_USERNAME = os.environ.get('UEC_DOS_USERNAME')
 UEC_DOS_PASSWORD = os.environ.get('UEC_DOS_PASSWORD')
 UEC_DOS_BASE_URL = os.environ.get('UEC_DOS_BASE_URL')
 
+SLACK_WEBHOOK_URL = os.environ.get('SLACK_WEBHOOK_URL')
+
 # SMS Settings
 TWILIO_ACCOUNT_SID = os.environ.get('TWILIO_ACCOUNT_SID')
 TWILIO_AUTH_TOKEN = os.environ.get('TWILIO_AUTH_TOKEN')
