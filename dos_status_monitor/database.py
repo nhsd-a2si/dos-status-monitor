c209d5ae5752bafa81cf1887c78ea8dd8fcb0bfb
Matt Stibbs <git@stibbsy.co.uk>
2017-12-24 11:33:45 +0000

    Add database methods to add, remove, and get watched services

--- dos_status_monitor/database.py
+++ dos_status_monitor/database.py
@@ -30,3 +30,26 @@ def get_snapshots_for_service(service_id):
     results = snapshots.find(query).sort([('checkTime',
                                            pymongo.DESCENDING)]).skip(1).limit(1)
     return results
+
+
+def get_service_watchlist():
+    logger.debug("Getting service watchlist from database")
+    query = {}
+    results = watched_services.find(query).sort([('id',
+                                                  pymongo.DESCENDING)])
+    return results
+
+
+def add_watched_service(service_id):
+    logger.debug(f'Adding watch for service {service_id}')
+    query = {'id': service_id}
+    update = {'id': service_id}
+    r = watched_services.update(query, update, upsert=True)
+    return r
+
+
+def remove_watched_service(service_id):
+    logger.debug(f'Removing watch for service {service_id}')
+    query = {'id': service_id}
+    r = watched_services.delete_many(query)
+    return r
