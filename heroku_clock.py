ba0cb7f2a8df382e9bbaa0d7845836eba91ca10c
Matt Stibbs <git@stibbsy.co.uk>
2017-12-29 15:57:44 +0000

    Refactoring and reformatting

--- heroku_clock.py
+++ heroku_clock.py
@@ -15,7 +15,7 @@ def add_search_job():
     
     for probe in probe_list:
         print("Adding job to queue")
-        q.enqueue(dos_status_monitor.run_search,
+        q.enqueue(dos_status_monitor.run_service_search,
                   probe,
                   ttl=f'{config.CHECK_RATE_MINUTES}m')
 
@@ -41,9 +41,12 @@ add_search_job()
 add_service_jobs()
 add_service_status_job()
 
-schedule.every(config.CHECK_RATE_MINUTES).minutes.do(add_search_job)
-schedule.every(config.CHECK_RATE_MINUTES).minutes.do(add_service_jobs)
-schedule.every(config.STATUS_UPDATE_RATE_MINUTES).minutes.do(add_service_status_job)
+schedule.every(config.CHECK_RATE_MINUTES).minutes\
+    .do(add_search_job)
+schedule.every(config.CHECK_RATE_MINUTES).minutes\
+    .do(add_service_jobs)
+schedule.every(config.STATUS_UPDATE_RATE_MINUTES).minutes\
+    .do(add_service_status_job)
 
 print(f"{len(probes.get_probe_list())} probes configured to run "
       f"every {config.CHECK_RATE_MINUTES} minute(s).")
