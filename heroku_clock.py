7c80ac1bff54790dee17d99242f79300440f331c
Matt Stibbs <git@stibbsy.co.uk>
2018-01-02 10:34:39 +0000

    Wrap entire clock process in a try/except so that Rollbar can catch exceptions

--- heroku_clock.py
+++ heroku_clock.py
@@ -9,60 +9,64 @@ from dos_status_monitor import logger
 
 rollbar.init(config.ROLLBAR_ACCESS_TOKEN, config.APP_NAME)
 
-# Set up RQ queue
-conn = redis.from_url(config.REDIS_URL)
-q = Queue(connection=conn)
+try:
 
+    # Set up RQ queue
+    conn = redis.from_url(config.REDIS_URL)
+    q = Queue(connection=conn)
 
-def add_search_jobs():
-    probe_list = probes.get_probe_list()
-    search_job_count = 0
-    
-    for probe in probe_list:
-        q.enqueue(dos_status_monitor.run_service_search,
-                  probe,
-                  ttl=f'{config.CHECK_RATE_MINUTES}m')
-        search_job_count += 1
+    def add_search_jobs():
+        probe_list = probes.get_probe_list()
+
+        search_job_count = 0
+
+        for probe in probe_list:
+            q.enqueue(dos_status_monitor.run_service_search,
+                      probe,
+                      ttl=f'{config.CHECK_RATE_MINUTES}m')
+            search_job_count += 1
 
         logger.info(f"{search_job_count} search probes configured to "
                     f"run every {config.CHECK_RATE_MINUTES} minute(s).")
 
+    def add_service_jobs():
+        service_list = probes.get_watched_service_list()
 
-def add_service_jobs():
-    service_list = probes.get_watched_service_list()
-    service_job_count = 0
+        service_job_count = 0
 
-    for service in service_list:
-        service_id = service['id']
-        q.enqueue(dos_status_monitor.check_single_service,
-                  service_id,
-                  ttl=f'{config.CHECK_RATE_MINUTES}m')
-        service_job_count += 1
+        for service in service_list:
+            service_id = service['id']
+            q.enqueue(dos_status_monitor.check_single_service,
+                      service_id,
+                      ttl=f'{config.CHECK_RATE_MINUTES}m')
+            service_job_count += 1
 
         logger.info(f"{service_job_count} service probes configured to "
                     f"run every {config.CHECK_RATE_MINUTES} minute(s).")
 
-def add_service_status_job():
-    q.enqueue(slack.send_slack_status_update,
-              ttl=f'{config.STATUS_UPDATE_RATE_MINUTES}m')
 
+    def add_service_status_job():
+        q.enqueue(slack.send_slack_status_update,
+                  ttl=f'{config.STATUS_UPDATE_RATE_MINUTES}m')
 
-add_search_jobs()
-add_service_jobs()
-add_service_status_job()
         logger.info("Slack Status Summary will run every "
                     f"{config.STATUS_UPDATE_RATE_MINUTES} minute(s).")
 
-schedule.every(config.CHECK_RATE_MINUTES).minutes\
-    .do(add_search_jobs)
-schedule.every(config.CHECK_RATE_MINUTES).minutes\
-    .do(add_service_jobs)
-schedule.every(config.STATUS_UPDATE_RATE_MINUTES).minutes\
-    .do(add_service_status_job)
 
+    add_search_jobs()
+    add_service_jobs()
+    add_service_status_job()
 
-print("Slack Status Summary will run every "
-      f"{config.STATUS_UPDATE_RATE_MINUTES} minute(s).")
+    # Set up the scheduled jobs
+    schedule.every(config.CHECK_RATE_MINUTES).minutes\
+        .do(add_search_jobs)
+    schedule.every(config.CHECK_RATE_MINUTES).minutes\
+        .do(add_service_jobs)
+    schedule.every(config.STATUS_UPDATE_RATE_MINUTES).minutes\
+        .do(add_service_status_job)
+
+except:
+    rollbar.report_exc_info()
 
 try:
     while True:
