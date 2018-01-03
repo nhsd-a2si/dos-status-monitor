c20c464fb3ffe1ff4f378ad4e304abdf58332b92
Matt Stibbs <git@stibbsy.co.uk>
2018-01-03 17:22:12 +0000

    Tweak logging for web process

--- api.py
+++ api.py
@@ -3,13 +3,14 @@ from flask import jsonify
 
 app = Flask(__name__)
 
-from dos_status_monitor import database
+from dos_status_monitor import database, logger
 
 
 @app.route('/statuses')
 def get_statuses():
     statuses = database.get_service_statuses()
-    print(statuses)
+    count = len(statuses)
+    logger.info(f"REQUEST /statuses {count} records")
     return jsonify(statuses)
 
 
