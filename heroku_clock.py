c69c2532ca9dbac57dfa75715e16efe37abb38a8
Matt Stibbs <git@stibbsy.co.uk>
2018-01-04 02:11:30 +0000

    Remove rollbar and update Tick to be an INFO log message

--- heroku_clock.py
+++ heroku_clock.py
@@ -6,69 +6,64 @@ from rq import Queue
 from dos_status_monitor import dos_status_monitor, probes, config, slack
 from dos_status_monitor import logger
 
-try:
-
-    # Set up RQ queue
-    conn = redis.from_url(config.REDIS_URL)
-    q = Queue(connection=conn)
-
-    def add_search_jobs():
-        probe_list = probes.get_probe_list()
-
-        search_job_count = 0
-
-        for probe in probe_list:
-            q.enqueue(dos_status_monitor.snapshot_service_search,
-                      probe,
-                      ttl=f'{config.CHECK_RATE_MINUTES}m')
-            search_job_count += 1
-
-        logger.info(f"{search_job_count} search probes configured to "
-                    f"run every {config.CHECK_RATE_MINUTES} minute(s).")
-
-    def add_service_jobs():
-        service_list = probes.get_watched_service_list()
-
-        service_job_count = 0
-
-        for service in service_list:
-            service_id = service['id']
-            q.enqueue(dos_status_monitor.snapshot_single_service,
-                      service_id,
-                      ttl=f'{config.CHECK_RATE_MINUTES}m')
-            service_job_count += 1
-
-        logger.info(f"{service_job_count} service probes configured to "
-                    f"run every {config.CHECK_RATE_MINUTES} minute(s).")
+# Set up RQ queue
+conn = redis.from_url(config.REDIS_URL)
+q = Queue(connection=conn)
 
 
-    def add_service_status_job():
-        q.enqueue(slack.send_slack_status_update,
-                  ttl=f'{config.STATUS_UPDATE_RATE_MINUTES}m')
+def add_search_jobs():
+    probe_list = probes.get_probe_list()
 
-        logger.info("Slack Status Summary will run every "
-                    f"{config.STATUS_UPDATE_RATE_MINUTES} minute(s).")
+    search_job_count = 0
+
+    for probe in probe_list:
+        q.enqueue(dos_status_monitor.snapshot_service_search,
+                  probe,
+                  ttl=f'{config.CHECK_RATE_MINUTES}m')
+        search_job_count += 1
+
+    logger.info(f"{search_job_count} search probes configured to "
+                f"run every {config.CHECK_RATE_MINUTES} minute(s).")
 
 
-    add_search_jobs()
-    add_service_jobs()
-    add_service_status_job()
+def add_service_jobs():
+    service_list = probes.get_watched_service_list()
 
-    # Set up the scheduled jobs
-    schedule.every(config.CHECK_RATE_MINUTES).minutes\
-        .do(add_search_jobs)
-    schedule.every(config.CHECK_RATE_MINUTES).minutes\
-        .do(add_service_jobs)
-    schedule.every(config.STATUS_UPDATE_RATE_MINUTES).minutes\
-        .do(add_service_status_job)
+    service_job_count = 0
 
-except:
-    rollbar.report_exc_info()
+    for service in service_list:
+        service_id = service['id']
+        q.enqueue(dos_status_monitor.snapshot_single_service,
+                  service_id,
+                  ttl=f'{config.CHECK_RATE_MINUTES}m')
+        service_job_count += 1
 
-try:
-    while True:
-        logger.debug(f"Ping!")
-        schedule.run_pending()
-        time.sleep(60)
-except:
-    rollbar.report_exc_info()
+    logger.info(f"{service_job_count} service probes configured to "
+                f"run every {config.CHECK_RATE_MINUTES} minute(s).")
+
+
+def add_service_status_job():
+    q.enqueue(slack.send_slack_status_update,
+              ttl=f'{config.STATUS_UPDATE_RATE_MINUTES}m')
+
+    logger.info("Slack Status Summary will run every "
+                f"{config.STATUS_UPDATE_RATE_MINUTES} minute(s).")
+
+
+add_search_jobs()
+add_service_jobs()
+add_service_status_job()
+
+# Set up the scheduled jobs
+schedule.every(config.CHECK_RATE_MINUTES).minutes\
+    .do(add_search_jobs)
+schedule.every(config.CHECK_RATE_MINUTES).minutes\
+    .do(add_service_jobs)
+schedule.every(config.STATUS_UPDATE_RATE_MINUTES).minutes\
+    .do(add_service_status_job)
+
+
+while True:
+    logger.info(f"Tick!")
+    schedule.run_pending()
+    time.sleep(60)
