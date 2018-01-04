5ba8e88ffdacf6907ba580456843b915bce139e2
Matt Stibbs <git@stibbsy.co.uk>
2018-01-04 11:32:08 +0000

    Add google maps api key to config

--- dos_status_monitor/config.py
+++ dos_status_monitor/config.py
@@ -46,3 +46,5 @@ ROLLBAR_ACCESS_TOKEN = os.environ.get('ROLLBAR_ACCESS_TOKEN', '')
 BASIC_AUTH_USERNAME = os.environ.get('BASIC_AUTH_USERNAME', 'user')
 BASIC_AUTH_PASSWORD = os.environ.get('BASIC_AUTH_PASSWORD', 'pass')
 BASIC_AUTH_FORCE = config_string_to_bool(os.environ.get('BASIC_AUTH_FORCE', 'True'))
+
+GOOGLE_MAPS_API_KEY = os.environ.get('GOOGLE_MAPS_API_KEY', '')
