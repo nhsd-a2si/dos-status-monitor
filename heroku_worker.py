70cdc1f1dd38cde01e7d0eb656a5c62af8ad6c7e
Matt Stibbs <git@stibbsy.co.uk>
2018-01-05 11:03:01 +0000

    Add Sentry support to RQ worker

--- heroku_worker.py
+++ heroku_worker.py
@@ -1,7 +1,11 @@
 import redis
 from rq import Worker, Queue, Connection
-import requests
-import pymongo
+from dos_status_monitor import config
+from raven import Client
+from raven.transport.http import HTTPTransport
+from rq.contrib.sentry import register_sentry
+
+client = Client(config.SENTRY_DSN, transport=HTTPTransport)
 
 from dos_status_monitor import config
 
@@ -11,5 +15,8 @@ conn = redis.from_url(config.REDIS_URL)
 
 if __name__ == '__main__':
     with Connection(conn):
+        import requests
+        import pymongo
         worker = Worker(map(Queue, listen))
+        register_sentry(client, worker)
         worker.work()
